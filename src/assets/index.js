16c0833d9eedbd67813c8f01070a0ed325d51f86
Rohan <rajputrohan196@gmail.com>
2024-05-29 18:06:01 +0530

    svg to png for checking

--- src/assets/index.js
+++ src/assets/index.js
@@ -66,7 +66,8 @@ import notification2 from "./notification/image-2.png";
 import notification3 from "./notification/image-3.png";
 import notification4 from "./notification/image-4.png";
 
-import benefitCard1 from "./benefits/card-1.svg";
+// import benefitCard1 from "./benefits/card-1.svg";
+import benefitCard1 from "./benefits/card-1.png";
 import benefitCard2 from "./benefits/card-2.svg";
 import benefitCard3 from "./benefits/card-3.svg";
 import benefitCard4 from "./benefits/card-4.svg";
